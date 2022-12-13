5f67957ce0b74e5f305fd4ff260894ae7033801a
Matt Post <mattpost@microsoft.com>
2022-12-13 14:58:48 -0800

    rewrote to detokenize with moses detokenizer

--- consistency_testsets/scoring_data/detok.py
+++ consistency_testsets/scoring_data/detok.py
@@ -3,31 +3,14 @@
 import re
 import sys
 
+from sacremoses import MosesDetokenizer
+
+detokenizer = MosesDetokenizer()
 
 def detok(line):
-    line = line.rstrip()
-
+    line = line.rstrip().split()
+    line = detokenizer.detokenize(line)
     line = line.replace(" _eos ", " <eos>")
-    line = line.replace(" . ", ". ")
-    line = line.replace(" ! ", "! ")
-    line = line.replace(" ? ", "? ")
-    line = line.replace(" , ", ", ")
-    line = line.replace(" ; ", "; ")
-    line = line.replace(" ! ", "! ")
-    line = line.replace(" ? ", "? ")
-    line = line.replace(" 's", "'s")
-    line = line.replace(" ' s", "'s")
-    line = line.replace(" 't", "'t")
-    line = line.replace(" 'm ", "'m ")
-    line = line.replace(" 've ", "'ve ")
-    line = line.replace(" 'll ", "'ll ")
-    line = line.replace(" 're ", "'re ")
-
-    line = re.sub(r'^(.*) " (.*) " (.*)', '\1 "\2" \3', line)
-
-    line = line.replace(" .", ".")
-    line = line.replace(" !", "!")
-    line = line.replace(" ?", "?")
 
     return line
 
